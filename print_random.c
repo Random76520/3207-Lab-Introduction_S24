2ce0bff60b346c190aa8c9fb07d2fb2e146a12a0
Random76520 <tun45218@temple.edu>
2024-01-17 14:56:20 -0500

    re-redo

--- print_random.c
+++ print_random.c
@@ -2,18 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 
-
-
-int main()
-{
+int main() {
 	int a;
 
 	/* seed the randomizer */
-	srand( (unsigned)time(NULL) );
+	srand((unsigned) time(NULL));
 
 	printf("Today's random word: ");
-	for(a=0;a<7;a++)
-		putchar( randchar() );
+	for (a = 0; a < 7; a++) {
+		putchar(randchar());
+	}
 	putchar('\n');
 
 	return(0);
---
+++ random.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int randchar() {
+	return (rand() %26) + 65;
+}
